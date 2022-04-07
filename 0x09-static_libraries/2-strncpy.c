e0a829d3b4565c413e96aae3707fb044c1be5308
MYENTY <jeffersonzouzou@hotmail.com>
2022-04-07 20:38:42 +0000

    new try

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strncpy - copy a string
+ * at most n bytes of src are copied
+ * @n: the maximum byte to copy
+ * @src: the string to copy n byte from
+ * @dest: the string where to paste what is copied
+ * Return:  pointer to the destination string dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+}
